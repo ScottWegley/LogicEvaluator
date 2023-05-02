7aa7bac401e56dbcbf4c9f5fdeb82bea3c8be3a9
Scott Wegley <scbrwegley@protonmail.ch>
2023-05-02 15:06:15 -0700

    main function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
 
+/**
+ * Our main function.  Used to establish initial state of page and setup all other event listeners.
+ */
+window.addEventListener('load', () => {
+    document.getElementById('exprForm').addEventListener('submit', (ev) => {
+        ev.preventDefault();
+    });
+});
