535f662ac8f9b550bc5dd73e696d576a13d176b8
Scott Wegley <scbrwegley@protonmail.ch>
2023-05-04 14:35:45 -0700

    moved similar functions to parent function for readibility

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,15 @@ import { Operations } from './expression.js';
  * Our main function.  Used to establish initial state of page and setup all other event listeners.
  */
 window.addEventListener('load', () => {
+    
+    injectScripts();
+});
+
+/**Adds event listeners to the HTML Elements on the page. */
+function injectScripts() {
+    /** Prevent the default behavior of submitting the form on button press. */
     document.getElementById('exprForm').addEventListener('submit', (ev) => {
         ev.preventDefault();
     });
-});
+}
+
